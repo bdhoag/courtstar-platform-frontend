f955d2b97ee2a745dfb5ec994360f569b0cfcb5c
thanhntse <nguyenthaithanh07@gmail.com>
2024-05-17 10:12:21 +0700

    add preline UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    './node_modules/preline/dist/preline.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
