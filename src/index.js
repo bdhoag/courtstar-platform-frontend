bb0dd95af63dc33f19868e1d54a1cb38dfb69471
zbububaiz <phongbu257@gmail.com>
2024-05-19 14:50:47 +0700

    add Register Page and edit Login Page

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import Footer from './Footer';
 import Banner from './Banner';
 import Centre from './Centre';
+import CustomerRegister from './CustomerRegister';
+import PartnerRegister from './PartnerRegister';
+import Login from './Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <Banner />
-    <Centre />
-    <Footer />
+    <Login />
   </React.StrictMode>
 );
 
