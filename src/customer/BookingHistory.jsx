50cd069b405d4f755e7226bc5e946e97699c2e1b
thanhntse <nguyenthaithanh07@gmail.com>
2024-05-29 17:19:20 +0700

    feat: add private route, athourize

--- src/customer/BookingHistory.jsx
+++ src/customer/BookingHistory.jsx
@@ -5,97 +5,97 @@ import centre from '../assets/images/demo-centre.png'
 
 const BookingHistory = () => {
 
-    const centreList = Array.from({ length: 10 }, (_, index) => index);
+  const centreList = Array.from({ length: 10 }, (_, index) => index);
 
-    return (
-        <div className='font-Inter text-base overflow-x-hidden text-gray-800'>
-            <div className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-gray-100">
-                <div className="max-w-screen-1440 1440:mx-auto mx-4 py-10 px-12 w-full flex flex-col gap-4 items-center justify-between">
-                    <div className='font-bold text-3xl uppercase text-start w-full pb-5 pl-2'>
-                        Booking History
+  return (
+    <div className='font-Inter text-base overflow-x-hidden text-gray-800'>
+      <div className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-gray-100">
+        <div className="max-w-screen-1440 1440:mx-auto mx-4 py-10 px-12 w-full flex flex-col gap-4 items-center justify-between">
+          <div className='font-bold text-3xl uppercase text-start w-full pb-5 pl-2'>
+            Booking History
+          </div>
+          <div className='flex gap-5 w-full bg-white rounded-2xl py-10'>
+            <div className='flex-1 flex flex-col gap-5 items-center'>
+              <div className='bg-white rounded-2xl shadow-lg border py-5 px-7 flex gap-7 max-w-5xl'>
+                <img src={centre}
+                  alt="demo centre"
+                  className='w-2/5'
+                />
+                <div className='flex flex-col gap-3'>
+                  <div className='font-semibold text-xl'>
+                    Sân cầu lông Đại học FPT Hồ Chí Minh
+                  </div>
+                  <div>
+                    <span className='font-semibold'>Address: </span>
+                    Lô E2a-7, Đường D1, Khu Công nghệ cao, P.Long Thạnh Mỹ, Tp. Thủ Đức, TP.HCM.
+                  </div>
+                  <div className='flex gap-3'>
+                    <div>
+                      <span className='font-semibold'>Time: </span>
+                      10h-12h
                     </div>
-                    <div className='flex gap-5 w-full bg-white rounded-2xl py-10'>
-                        <div className='flex-1 flex flex-col gap-5 items-center'>
-                            <div className='bg-white rounded-2xl shadow-lg border py-5 px-7 flex gap-7 max-w-5xl'>
-                                <img src={centre}
-                                    alt="demo centre"
-                                    className='w-2/5'
-                                />
-                                <div className='flex flex-col gap-3'>
-                                    <div className='font-semibold text-xl'>
-                                        Sân cầu lông Đại học FPT Hồ Chí Minh
-                                    </div>
-                                    <div>
-                                        <span className='font-semibold'>Address: </span>
-                                        Lô E2a-7, Đường D1, Khu Công nghệ cao, P.Long Thạnh Mỹ, Tp. Thủ Đức, TP.HCM.
-                                    </div>
-                                    <div className='flex gap-3'>
-                                        <div>
-                                            <span className='font-semibold'>Time: </span>
-                                            10h-12h
-                                        </div>
-                                        <div>
-                                            <span className='font-semibold'>Court number: </span>
-                                            2
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <span className='font-semibold'>Total price: </span>
-                                        <span className='font-semibold text-rose-600'>200.000₫/h</span>
-                                    </div>
-                                    <div className='text-sm flex justify-center gap-20'>
-                                        <button className='block text-center py-1 w-40 border bg-gray-800 text-white rounded-md font-semibold hover:bg-gray-950 transition-all ease-in-out duration-300'>
-                                            Feedback
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            {centreList.map((_, index) => (
-                                <div key={index}
-                                    className='bg-white rounded-2xl shadow-lg border py-5 px-7 flex gap-7 max-w-5xl'>
-                                    <img src={centre}
-                                        alt="demo centre"
-                                        className='w-2/5'
-                                    />
-                                    <div className='flex flex-col gap-3'>
-                                        <div className='font-semibold text-xl'>
-                                            Sân cầu lông Đại học FPT Hồ Chí Minh
-                                        </div>
-                                        <div>
-                                            <span className='font-semibold'>Address: </span>
-                                            Lô E2a-7, Đường D1, Khu Công nghệ cao, P.Long Thạnh Mỹ, Tp. Thủ Đức, TP.HCM.
-                                        </div>
-                                        <div className='flex gap-3'>
-                                            <div>
-                                                <span className='font-semibold'>Time: </span>
-                                                10h-12h
-                                            </div>
-                                            <div>
-                                                <span className='font-semibold'>Court number: </span>
-                                                2
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <span className='font-semibold'>Total price: </span>
-                                            <span className='font-semibold text-rose-600'>200.000₫/h</span>
-                                        </div>
-                                        <div className='text-sm flex justify-center gap-20'>
-                                            <div className='flex gap-1'>
-                                                <img src={star} alt="Star" className='cursor-pointer w-5' />
-                                                <img src={star} alt="Star" className='cursor-pointer w-5' />
-                                                <img src={star} alt="Star" className='cursor-pointer w-5' />
-                                                <img src={star} alt="Star" className='cursor-pointer w-5' />
-                                                <img src={nostar} alt="NoStar" className='cursor-pointer w-5' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+                    <div>
+                      <span className='font-semibold'>Court number: </span>
+                      2
                     </div>
+                  </div>
+                  <div>
+                    <span className='font-semibold'>Total price: </span>
+                    <span className='font-semibold text-rose-600'>200.000₫/h</span>
+                  </div>
+                  <div className='text-sm flex justify-center gap-20'>
+                    <button className='block text-center py-1 w-40 border bg-gray-800 text-white rounded-md font-semibold hover:bg-gray-950 transition-all ease-in-out duration-300'>
+                      Feedback
+                    </button>
+                  </div>
                 </div>
+              </div>
+              {centreList.map((_, index) => (
+                <div key={index}
+                  className='bg-white rounded-2xl shadow-lg border py-5 px-7 flex gap-7 max-w-5xl'>
+                  <img src={centre}
+                    alt="demo centre"
+                    className='w-2/5'
+                  />
+                  <div className='flex flex-col gap-3'>
+                    <div className='font-semibold text-xl'>
+                      Sân cầu lông Đại học FPT Hồ Chí Minh
+                    </div>
+                    <div>
+                      <span className='font-semibold'>Address: </span>
+                      Lô E2a-7, Đường D1, Khu Công nghệ cao, P.Long Thạnh Mỹ, Tp. Thủ Đức, TP.HCM.
+                    </div>
+                    <div className='flex gap-3'>
+                      <div>
+                        <span className='font-semibold'>Time: </span>
+                        10h-12h
+                      </div>
+                      <div>
+                        <span className='font-semibold'>Court number: </span>
+                        2
+                      </div>
+                    </div>
+                    <div>
+                      <span className='font-semibold'>Total price: </span>
+                      <span className='font-semibold text-rose-600'>200.000₫/h</span>
+                    </div>
+                    <div className='text-sm flex justify-center gap-20'>
+                      <div className='flex gap-1'>
+                        <img src={star} alt="Star" className='cursor-pointer w-5' />
+                        <img src={star} alt="Star" className='cursor-pointer w-5' />
+                        <img src={star} alt="Star" className='cursor-pointer w-5' />
+                        <img src={star} alt="Star" className='cursor-pointer w-5' />
+                        <img src={nostar} alt="NoStar" className='cursor-pointer w-5' />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 export default BookingHistory;
