160bde1189fec30fee9f1e1d818ca59780249577
thanhntse <nguyenthaithanh07@gmail.com>
2024-05-31 11:18:35 +0700

    feat: update role

--- src/page/Header.jsx
+++ src/page/Header.jsx
@@ -95,12 +95,12 @@ const Header = () => {
               <Link className="text-gray-200 hover:text-white transition-all ease-in-out duration-300"
               to="/partnerRegister">Partner Register</Link>
               {
-                role === 'ADMIN' &&
+                role?.includes('ADMIN') &&
                 <Link className="text-gray-200 hover:text-white transition-all ease-in-out duration-300"
                 to="/admin">My Dashboard</Link>
               }
               {
-                (role === 'ADMIN' || role === 'CENTRE_MANAGER' || role === 'CENTRE_STAFF') &&
+                (role?.includes('ADMIN') || role?.includes('CENTRE_MANAGER') || role?.includes('CENTRE_MANAGER')) &&
                 <Link className="text-gray-200 hover:text-white transition-all ease-in-out duration-300"
                 to="/myCentre">My Centre</Link>
               }
--- src/routes/ManagerRoute.jsx
+++ src/routes/ManagerRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 const ManagerRoute = ({ children }) => {
   const userRole = localStorage.getItem('role');
 
-  if (!userRole || userRole === 'CUSTOMER') {
+  if (!userRole || userRole.includes('CUSTOMER')) {
     return <Navigate to="/" />;
   }
 
