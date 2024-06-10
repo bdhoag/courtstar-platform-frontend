9f089ae1b973316b60a07fbd5d46f55795e3bfb0
Koolice <tiensang2801@gmail.com>
2024-06-10 23:48:43 +0700

    Filter Search by District

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Centre from './centre/Centre';
 import Banner from './centre/Banner';
 
 const Home = () => {
+  const [selectedDistrict, setSelectedDistrict] = useState('');
+
+  const handleDistrictSelect = (district) => {
+    setSelectedDistrict(district);
+  };
+
   return (
     <div>
-      <Banner />
-      <Centre />
+      <Banner onDistrictSelect={handleDistrictSelect} />
+      <Centre selectedDistrict={selectedDistrict} />
     </div>
   );
-}
+};
 
 export default Home;
