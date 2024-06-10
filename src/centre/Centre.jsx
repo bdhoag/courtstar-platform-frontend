dee0692dde70e5e57c4dd4ebc77fe14b97d03202
thanhntse <nguyenthaithanh07@gmail.com>
2024-06-10 07:32:08 +0700

    refactor: to locale string

--- src/centre/Centre.jsx
+++ src/centre/Centre.jsx
@@ -120,7 +120,7 @@ const Centre = () => {
                       <div>
                         <span className='font-semibold'>Price: </span>
                         <span className='font-semibold text-rose-600'>
-                          {centre.pricePerHour}₫/h
+                          {centre.pricePerHour.toLocaleString('de-DE')} VND/h
                         </span>
                       </div>
                       <div className='text-sm flex justify-center gap-20'>
