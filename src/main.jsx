2c5b2bd00eb33132c85f195f19789435bd47505e
thanhntse <nguyenthaithanh07@gmail.com>
2024-06-15 14:27:43 +0700

    fix: bug trigger twice

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,5 @@ import './assets/css/index.css';
 import './assets/css/scrollbar.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
