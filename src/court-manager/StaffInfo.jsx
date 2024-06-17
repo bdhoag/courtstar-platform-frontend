c479ab028ac2d26e7891f6c2b1793bfe51a54f05
Hoang Bui Duc <hoangakiy@gmail.com>
2024-06-17 10:52:45 +0700

    feat: reload realtime

--- src/court-manager/StaffInfo.jsx
+++ src/court-manager/StaffInfo.jsx
@@ -112,7 +112,7 @@ function StaffInfo() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-              class="lucide lucide-users-round">
+              className="lucide lucide-users-round">
               <path d="M18 21a8 8 0 0 0-16 0" />
               <circle cx="10" cy="8" r="5" />
               <path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3" />
