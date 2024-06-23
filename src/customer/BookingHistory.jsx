8334a3549d63bfe4575385bc8b306e2db05dd368
Koolice <tiensang2801@gmail.com>
2024-06-23 23:25:41 +0700

    SpinnerLoading bookhistory

--- src/customer/BookingHistory.jsx
+++ src/customer/BookingHistory.jsx
@@ -5,13 +5,15 @@ import FeedbackForm from './FeedbackForm';
 import Rating from '../components/Rating';
 import moment from 'moment';
 import Pagination from '../components/pagination';
+import SpinnerLoading from '../components/SpinnerLoading';
 
 const BookingHistory = () => {
   const [feedbackPopup, setFeedbackPopup] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [bookings, setBookings] = useState([]);
   const [selectedBooking, setSelectedBooking] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4; 
+  const itemsPerPage = 4;
 
   // Fetch booking data from the API
   const fetchBookings = async () => {
@@ -30,6 +32,8 @@ const BookingHistory = () => {
       }
     } catch (error) {
       console.error('Error fetching booking data:', error);
+    } finally {
+      setLoading(false); 
     }
   };
 
@@ -69,99 +73,103 @@ const BookingHistory = () => {
           <div className="font-bold text-3xl uppercase text-start w-full pb-5 pl-2">
             Booking History
           </div>
-          <div className="flex gap-5 w-full bg-white rounded-2xl py-10">
-            <div className="flex-1 flex flex-col gap-5 items-center">
-              {currentBookings.length 
-              ? (
-                <>
-                  {currentBookings.map((booking) => (
-                    <div
-                      key={booking.id}
-                      className="bg-white rounded-2xl shadow-2xl border py-5 px-7 flex gap-7 max-w-5xl w-full"
-                    >
-                      <img
-                        src={booking.centreImg || centrePlaceholder}
-                        alt={booking.centreName}
-                        className="min-w-80 max-w-80 h-56 rounded-lg object-cover object-center"
-                      />
-                      <div className="flex flex-col gap-3 w-full">
-                        <div className="font-semibold text-xl">
-                          {booking.centreName}
-                        </div>
-                        <div>
-                          <span className="font-semibold">Address: </span>
-                          {booking.centreAddress}
-                        </div>
-                        <div>
-                          <span className="font-semibold">Date: </span>
-                          {booking.date}
-                        </div>
-                        <div className="flex gap-3">
-                          <div>
-                            <span className="font-semibold">Time: </span>
-                            {moment(booking.slot.startTime, 'HH:mm:ss').format('HH:mm')} - {moment(booking.slot.endTime, 'HH:mm:ss').format('HH:mm')}
+          <div className="flex gap-5 w-full bg-white rounded-2xl py-10 min-h-[50px]"> 
+            {loading ? (
+              <SpinnerLoading type="page" height="80" width="80" color='#2B5A50'/>
+            ) : (
+              <div className="flex-1 flex flex-col gap-5 items-center">
+                {currentBookings.length 
+                ? (
+                  <>
+                    {currentBookings.map((booking) => (
+                      <div
+                        key={booking.id}
+                        className="bg-white rounded-2xl shadow-2xl border py-5 px-7 flex gap-7 max-w-5xl w-full"
+                      >
+                        <img
+                          src={booking.centreImg || centrePlaceholder}
+                          alt={booking.centreName}
+                          className="min-w-80 max-w-80 h-56 rounded-lg object-cover object-center"
+                        />
+                        <div className="flex flex-col gap-3 w-full">
+                          <div className="font-semibold text-xl">
+                            {booking.centreName}
                           </div>
                           <div>
-                            <span className="font-semibold">Court number: </span>
-                            {booking.court.courtNo}
+                            <span className="font-semibold">Address: </span>
+                            {booking.centreAddress}
                           </div>
-                        </div>
-                        <div>
-                          <span className="font-semibold">Total price: </span>
-                          <span className="font-semibold text-rose-600">
-                            {booking.totalPrice.toLocaleString('vi-VN', {
-                              style: 'currency',
-                              currency: 'VND',
-                            })}
-                          </span>
-                        </div>
-                        <div className="flex justify-center items-center h-full">
-                          {booking.rate === 0 ? (
-                            <button
-                              className="block text-center py-1 w-full border bg-primary-green text-white rounded-md font-semibold hover:bg-teal-900 transition-all ease-in-out duration-300"
-                              onClick={() => handleFeedbackPopup(booking)}
-                            >
-                              Feedback
-                            </button>
-                          ) : (
-                            <Rating
-                              ratingWrapper="flex gap-1"
-                              value={booking.rate}
-                              editable={false}
-                            />
-                          )}
+                          <div>
+                            <span className="font-semibold">Date: </span>
+                            {booking.date}
+                          </div>
+                          <div className="flex gap-3">
+                            <div>
+                              <span className="font-semibold">Time: </span>
+                              {moment(booking.slot.startTime, 'HH:mm:ss').format('HH:mm')} - {moment(booking.slot.endTime, 'HH:mm:ss').format('HH:mm')}
+                            </div>
+                            <div>
+                              <span className="font-semibold">Court number: </span>
+                              {booking.court.courtNo}
+                            </div>
+                          </div>
+                          <div>
+                            <span className="font-semibold">Total price: </span>
+                            <span className="font-semibold text-rose-600">
+                              {booking.totalPrice.toLocaleString('vi-VN', {
+                                style: 'currency',
+                                currency: 'VND',
+                              })}
+                            </span>
+                          </div>
+                          <div className="flex justify-center items-center h-full">
+                            {booking.rate === 0 ? (
+                              <button
+                                className="block text-center py-1 w-full border bg-primary-green text-white rounded-md font-semibold hover:bg-teal-900 transition-all ease-in-out duration-300"
+                                onClick={() => handleFeedbackPopup(booking)}
+                              >
+                                Feedback
+                              </button>
+                            ) : (
+                              <Rating
+                                ratingWrapper="flex gap-1"
+                                value={booking.rate}
+                                editable={false}
+                              />
+                            )}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
-                  <Pagination
-                    totalItems={bookings.length}
-                    itemsPerPage={itemsPerPage}
-                    currentPage={currentPage}
-                    onPageChange={handlePageChange}
-                  />
-                </>
-              ) : (
-                <div className="flex flex-col items-center justify-center h-56 text-3xl text-primary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="150" height="150"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-ticket-x"
-                  >
-                    <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
-                    <path d="m9.5 14.5 5-5" />
-                    <path d="m9.5 9.5 5 5" />
-                  </svg>
-                  You don't have any bookings yet!
-                </div>
-              )}
-            </div>
+                    ))}
+                    <Pagination
+                      totalItems={bookings.length}
+                      itemsPerPage={itemsPerPage}
+                      currentPage={currentPage}
+                      onPageChange={handlePageChange}
+                    />
+                  </>
+                ) : (
+                  <div className="flex flex-col items-center justify-center h-56 text-3xl text-primary">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="150" height="150"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-ticket-x"
+                    >
+                      <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
+                      <path d="m9.5 14.5 5-5" />
+                      <path d="m9.5 9.5 5 5" />
+                    </svg>
+                    You don't have any bookings yet!
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
